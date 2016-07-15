d33c0b9bd55928a996373c5be43e8ddf851928d9
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-15 10:48:14 +0900

    Add a test case

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,51 @@
+{-# LANGUAGE QuasiQuotes, StandaloneDeriving #-}
+
+import Data.Data
+import Data.Ini
+import Data.Ini.QQ
+import qualified Data.Text as T
+import System.Exit
+import Text.RawString.QQ
+import Test.HUnit
+
+deriving instance Eq Ini
+
+testIni :: Ini
+testIni = [ini|
+# Some comment.
+[SERVER]
+port=6667
+hostname=localhost
+; another comment here
+[AUTH]
+user: hello
+pass: world
+salt:|]
+
+expectedIni :: Ini
+expectedIni =
+  case parsed of
+    Left err -> error $ "Error in parsing ini: " ++ show err
+    Right val -> val
+  where parsed = parseIni (T.pack [r|
+# Some comment.
+[SERVER]
+port=6667
+hostname=localhost
+; another comment here
+[AUTH]
+user: hello
+pass: world
+salt:|])
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ test [
+  "Quasiquote for ini" ~: (expectedIni ~=? testIni)
+  ]
+
+defaultMain :: Test -> IO ()
+defaultMain t = do
+  cnts <- runTestTT t
+  case failures cnts + errors cnts of
+    0 -> exitWith $ ExitSuccess
+    n -> exitWith $ ExitFailure n
